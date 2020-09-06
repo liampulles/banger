c26872b7f927d3d96c6ffbd7d60b9fb68e3258af
Liam Pulles <me@liampulles.com>
2020-09-06 03:10:25 +0200

    Project basis

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/liampulles/banger
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("hot damn, dem some good tunes...")
+	os.Exit(0)
+}
